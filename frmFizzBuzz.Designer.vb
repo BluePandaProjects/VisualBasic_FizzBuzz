eccf6c24d410fc4512f310d62a06f3bfaaa54730
BluePandaProjects <willdridge@hotmail.com>
2021-11-02 16:03:41 +0000

    Very basic functionality

--- frmFizzBuzz.Designer.vb
+++ frmFizzBuzz.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmFizzBuzz
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,64 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.textboxInputVale = New System.Windows.Forms.TextBox()
+        Me.labelInputValue = New System.Windows.Forms.Label()
+        Me.listviewResults = New System.Windows.Forms.ListView()
+        Me.buttonFizzBuzz = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'textboxInputVale
+        '
+        Me.textboxInputVale.Location = New System.Drawing.Point(82, 12)
+        Me.textboxInputVale.Name = "textboxInputVale"
+        Me.textboxInputVale.Size = New System.Drawing.Size(100, 20)
+        Me.textboxInputVale.TabIndex = 0
+        '
+        'labelInputValue
+        '
+        Me.labelInputValue.AutoSize = True
+        Me.labelInputValue.Location = New System.Drawing.Point(12, 15)
+        Me.labelInputValue.Name = "labelInputValue"
+        Me.labelInputValue.Size = New System.Drawing.Size(47, 13)
+        Me.labelInputValue.TabIndex = 1
+        Me.labelInputValue.Text = "Number:"
+        '
+        'listviewResults
+        '
+        Me.listviewResults.HideSelection = False
+        Me.listviewResults.Location = New System.Drawing.Point(12, 38)
+        Me.listviewResults.Name = "listviewResults"
+        Me.listviewResults.Size = New System.Drawing.Size(776, 400)
+        Me.listviewResults.TabIndex = 2
+        Me.listviewResults.UseCompatibleStateImageBehavior = False
+        '
+        'buttonFizzBuzz
+        '
+        Me.buttonFizzBuzz.Location = New System.Drawing.Point(713, 10)
+        Me.buttonFizzBuzz.Name = "buttonFizzBuzz"
+        Me.buttonFizzBuzz.Size = New System.Drawing.Size(75, 23)
+        Me.buttonFizzBuzz.TabIndex = 3
+        Me.buttonFizzBuzz.Text = "FizzBuzz"
+        Me.buttonFizzBuzz.UseVisualStyleBackColor = True
+        '
+        'frmFizzBuzz
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.buttonFizzBuzz)
+        Me.Controls.Add(Me.listviewResults)
+        Me.Controls.Add(Me.labelInputValue)
+        Me.Controls.Add(Me.textboxInputVale)
+        Me.Name = "frmFizzBuzz"
+        Me.Text = "FizzBuzz - BluePandaProjects"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents textboxInputVale As TextBox
+    Friend WithEvents labelInputValue As Label
+    Friend WithEvents listviewResults As ListView
+    Friend WithEvents buttonFizzBuzz As Button
 End Class
--- frmFizzBuzz.vb
+++ frmFizzBuzz.vb
@@ -1,3 +1,50 @@
-﻿Public Class Form1
+﻿Public Class frmFizzBuzz
+    Private FIZZ As String = "fizz"
+    Private BUZZ As String = "buzz"
 
+    Dim inputValue As Integer = 0
+    Private Sub frmFizzBuzz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function CheckIsInteger(value As Object)
+        Dim resultValue As Integer = -1
+
+        If Integer.TryParse(value, resultValue) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub buttonFizzBuzz_Click(sender As Object, e As EventArgs) Handles buttonFizzBuzz.Click
+        Dim rawString As String = textboxInputVale.Text
+        Dim outputText As String = ""
+
+        listviewResults.Columns.Add("FizzBuzzResult")
+
+        If CheckIsInteger(rawString) Then
+            Dim targetNumber As Integer = CInt(rawString)
+
+            For currentNumber As Integer = 1 To targetNumber
+                listviewResults.Items.Add(FizzBuzz(currentNumber))
+            Next
+        Else
+            MsgBox("Input Value must be a whole number")
+        End If
+    End Sub
+
+    Private Function FizzBuzz(targetNumber As Integer) As String
+        Dim result As String = ""
+
+        If targetNumber Mod 3 = 0 Then
+            listviewResults.Items.Add(FIZZ)
+        ElseIf targetNumber Mod 5 = 0 Then
+            listviewResults.Items.Add(BUZZ)
+        Else
+            listviewResults.Items.Add(targetNumber.ToString)
+        End If
+
+        Return result
+    End Function
 End Class
