3e517514475223ece852fd4e85c132742ba7d367
BluePandaProjects <willdridge@hotmail.com>
2021-11-02 18:22:50 +0000

    Working FizzBuzz with ListView

--- frmFizzBuzz.vb
+++ frmFizzBuzz.vb
@@ -4,7 +4,13 @@
 
     Dim inputValue As Integer = 0
     Private Sub frmFizzBuzz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With listviewResults
 
+            .View = View.Details
+            .Columns.Add("FizzBuzz", 100)
+        End With
+        'listviewResults.Columns.Add("FizzBuzz", 100)
+        'listviewResults.Columns.Add("TestABC", 100)
     End Sub
 
     Private Function CheckIsInteger(value As Object)
@@ -21,13 +27,15 @@
         Dim rawString As String = textboxInputVale.Text
         Dim outputText As String = ""
 
-        listviewResults.Columns.Add("FizzBuzzResult")
-
         If CheckIsInteger(rawString) Then
             Dim targetNumber As Integer = CInt(rawString)
 
             For currentNumber As Integer = 1 To targetNumber
-                listviewResults.Items.Add(FizzBuzz(currentNumber))
+                Dim listViewItem As New ListViewItem
+
+                listViewItem.Text = FizzBuzz(currentNumber)
+
+                listviewResults.Items.Add(listViewItem)
             Next
         Else
             MsgBox("Input Value must be a whole number")
@@ -37,12 +45,14 @@
     Private Function FizzBuzz(targetNumber As Integer) As String
         Dim result As String = ""
 
-        If targetNumber Mod 3 = 0 Then
-            listviewResults.Items.Add(FIZZ)
+        If targetNumber Mod 3 = 0 And targetNumber Mod 5 = 0 Then
+            result = FIZZ.ToUpper & BUZZ.ToUpper
+        ElseIf targetNumber Mod 3 = 0 Then
+            result = FIZZ
         ElseIf targetNumber Mod 5 = 0 Then
-            listviewResults.Items.Add(BUZZ)
+            result = BUZZ
         Else
-            listviewResults.Items.Add(targetNumber.ToString)
+            result = targetNumber.ToString
         End If
 
         Return result
